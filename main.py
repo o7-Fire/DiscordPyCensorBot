6f522abd05f444b64a02a9265584ea2b8ac54cb5
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-08-05 19:06:19 +0200

    allow anime

--- main.py
+++ main.py
@@ -3,28 +3,29 @@ import traceback
 import keep_alive
 keep_alive.keep_alive()
 ##assad your private key is exposed
-import dialogflow
-from google.api_core.exceptions import InvalidArgument
 import os
 import discord
 import unicodedata
 import json
 import sys
-import urllib.request
 import requests
 import operator
 import re
-from monkeylearn import MonkeyLearn
+
 
 def restart_program():
     python = sys.executable
     os.execl(python, python, *sys.argv)
+#Submit async function
+import asyncio
+def submitTask(task):
+  return asyncio.get_running_loop().create_task(task)
 
 
 isTextCensorEnabled = 0
 isSpamAllowed = 0
 try:
-    with open("spamsettings.txt", "r+") as f:
+    with open("spadel msettings.txt", "r+") as f:
         isSpamAllowed = f.read()
 except FileNotFoundError:
     print("hey spamsettings.txt is not found")
@@ -36,11 +37,10 @@ except FileNotFoundError:
 
 print("VALUE OF ISSPAMALLOWED: " + str(isSpamAllowed))
 print("VALUE OF ISTEXTCENSORENABLED: " + str(isTextCensorEnabled))
-ml = MonkeyLearn(os.getenv('ML_TOKEN'))
 TOKEN = os.getenv('BOT_TOKEN')  # for now use glitch api since volas having problem
 ##https://o7-api.glitch.me
 ##https://o7inc.ddns.net
-o7API = "https://o7-nsfw-api.glitch.me"  # choose your api
+o7API = os.getenv("o7API")  # choose your api
 client = discord.Client()  # finally migrated to o7inc.ddns.net api
 spam = {}
 maxhandleReadableContent = 0.25  # 0 - 1. read async def handleReadableContent
@@ -50,9 +50,14 @@ thresholdNeutralizer = 0.017  # round to 0 automatically
 thresholdMaximizer = 0.81  # if the index above this and is safe dont delete
 safeIndex = ["Neutral"]  # prevent false positive, never trust Drawing
 neutralIndex = ['Neutral']#Not worth checking
-badIndex = ["Porn", "Sexy", "Hentai", "Anime"]
+badIndex = ["Porn", "Sexy", "Hentai"]
+censored_words = ["suck me", "suck ne", "masterbat",
+                  "horny", "lesbian", "bisexual", "vagina", "penis", "cock", "mastorbat",
+                  "hentai", "henthai", "hxntai", "hormy", "fuck ne", "masturbat",
+                  "lets sex", "porn", "daddy", "porm", "fuck me", "anal", "buttplug",
+                  ":woozy_face:", ":flushed:", ":drooling_face:", "rape", "nigger", "nigga"]  # 343591759332245505
 whitelisted_users = [7706075274265231707, 753874678220849174, 332394297536282634]
-urlDiscordMedia = re.compile("((https|http):\/\/[0-9a-zA-Z\.\/_-]+.(png|jpg|gif|webm|mp4|jpeg))")
+urlDiscordMedia = re.compile("((https|http):\/\/[0-9a-zA-Z\.\/_-]+.(png|jpg|gif|webm|mp4|jpeg|webm))")
 
 DIALOGFLOW_PROJECT_ID = os.getenv('PROJECT_ID')
 DIALOGFLOW_LANGUAGE_CODE = 'en'
@@ -64,8 +69,13 @@ async def on_ready():
 
 
 def getClassification(img: str):
-    content = keep_alive.fetch(o7API + '/api/json/graphical/classification/' + img)  
+  content = img
+  try:
+    content = keep_alive.fetch(o7API + '/api/json/graphical/classification/' + img)
+    print(o7API + '/api/json/graphical/classification/' + img)
     return json.loads(content)
+  except Exception as e:
+    return {'err': str(e), 'res': content}
 
 #weeb problems require weeb solution
 async def handleReadableContent(message, content: str, value: float, debug: bool):
@@ -112,9 +122,19 @@ async def checkVisual(message):
         return
     urls: list = urlDiscordMedia.findall(message.content)
     done: list = []
-    for img in message.attachments:
+    for emb in message.embeds:
+      img = emb.image
+      if img is not discord.Embed.Empty:
+        img = img.url
+        if not isinstance(img, str):
+          continue
         if done.__contains__(img): continue
         done.append(img)
+        if await checkVisualF(message, img):
+            return True
+    for img in message.attachments:
+        if done.__contains__(img.url): continue
+        done.append(img.url)
         if await checkVisualF(message, img.url):
             return True
     for u in urls:
@@ -129,6 +149,12 @@ async def checkVisualF(message, img):
     try:
         debug = "debug" in message.content
         rawResponse: dict = getClassification(img)  # json parser from api
+        print('resp')
+        print(rawResponse)
+        if('err' in rawResponse or 'error' in rawResponse):
+          if debug:
+            await message.channel.send(str(rawResponse))
+          return
         contents: dict = rawResponse.copy()
         contents["model"] = 0
         contents = dict(sorted(contents.items(), key=operator.itemgetter(1), reverse=True))  # sort float value
@@ -202,56 +228,54 @@ async def checkSpam(message):
     return False
 
 async def handleBot(message):
- if message.channel.id == 815743358639013958:
+ if message.channel.id == 840041811384860709:
       contents = requests.get(f'https://api.affiliateplus.xyz/api/chatbot?message={message.content}&botname=o7 AI Bot&ownername=Nexity&user=1').text
       cleanup = contents.split(":")[1].replace('"', "").replace("}", "")
       await message.channel.send(cleanup)
-
-async def handleText(message):
-  content = message.content
-  model_id = 'cl_fobQ7iGo'
-  data = [content]
-  result = ml.classifiers.classify(model_id, data)
-  for items in result.body:
-    for item in items['classifications']:
-      #await message.channel.send(str(item) + str(type(item)))
-      if item['tag_name'] == "NSFW":
-        await message.channel.send(f'<@{message.author.id}> Your message was detected to be {item["tag_name"]}\nProbability {item["confidence"]}')
         
 async def handleTextG(message):
-  r = requests.get(f'https://api.nexitysecond.repl.co/nsfwclassify?text={message.content}').text
+  r = requests.get(f'https://api.nexitysecond.repl.co/nsfwclassify?text={unicodedata.normalize("NFKC", message.content)}').text
   e = r.replace("'", "").split(', ')
   intent = e[1].replace("intent: ", "")
   confidence = e[2].replace("confidence: ", "").replace("}", "")
   if intent == "NSFW intent":
-    if float(confidence) > 0.5:
+    if float(confidence) > thresholdMaximizer:
       await message.delete()
       await message.channel.send(f"""<@{message.author.id}> Your message was checked and is probably NSFW
 Detected intent confidence:  {confidence}""")
-      # FEEDBACK
-      try:
-        asd = client.get_channel(0123456789012345)
-        await asd.send(f"<@343591759332245505> https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id}\n```channel: {message.channel.name} ({message.channel.id})\nmessage: {message.content}\nintent confidence: {response.query_result.intent_detection_confidence}```")
-      except:
-        print("It seems as you did not set a channel for feedback. Invite this bot to a server, find a channel for the feedback, and change the client.get_channel(0123456789012345) to the channel id.")               
-    else:
-      await message.channel.send(f"""<@{message.author.id}> Your message was checked and is maybe NSFW
+      asd = client.get_channel(828189106911182898)
+      #print(message.links)https://discord.com/channels/651737864593211394/801061985034305556/828195273992962058
+      if float(confidence) < 0.9:
+       await asd.send(f"<@343591759332245505> https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id}\n```channel: {message.channel.name} ({message.channel.id})\nmessage: {message.content}\nintent confidence: {confidence}```")
+    elif float(confidence) > (threshold):
+      await message.channel.send(f""" <@{message.author.id}> Your message was checked and is maybe NSFW
 Detected intent confidence:  {confidence}""")
-
+ 
+@client.event
+async def on_message_edit(before, after):
+  await on_message(after)
 
 @client.event
 async def on_message(message):
     global isSpamAllowed
     global isTextCensorEnabled
     if message.author.id == 0:  # if you hate someone really bad and you cant ban him from a server because the state doesnt let you
-        await message.delete()
-
-    if message.author.bot:
+        submitTask(message.delete())
         return
-    # DONT DELETE
-    await basicHandle(message)
-    await handleBot(message)
-    await handleTextG(message)
+    if message.author.id == client.user.id:
+        return
+
+    if message.content.startswith("whitelist") and message.author.id == 761484355084222464:
+      with open('whitelisted.txt', 'w+') as f:
+        f.write("\n" + message.content.split(' ')[1])
+        await message.channel.send('whitelisted ' + message.content.split(' ')[1])
+    if message.author.id in whitelisted_users and not str(message.content).startswith("debug"):
+        return
+    
+    if not message.content == "debug" and not message.author.bot:
+        if await checkSpam(message):
+            return
+    submitTask(checkVisual(message))
 
     if message.content == "switchspam":
         if message.author.id in whitelisted_users or message.author.id == 343591759332245505:
@@ -265,7 +289,7 @@ async def on_message(message):
                 with open("spamsettings.txt", "w+") as f:
                     f.write(str(isSpamAllowed))
                 await message.channel.send("turned off antispam")
-
+    
     if message.content == "switchtextcensor":
         if message.author.id in whitelisted_users or message.author.id == 343591759332245505:
             if isTextCensorEnabled:
@@ -282,15 +306,33 @@ async def on_message(message):
     if "is AFK" in message.content and message.author.id == 155149108183695360:
         await message.delete()
 
-    if message.author.id in whitelisted_users and not str(message.content).startswith("debug"):
+    with open('whitelisted.txt', 'r') as f:
+      if str(message.author.id) in f.read():
         return
 
-    if not message.content == "debug":
-        if await checkSpam(message):
+    # DONT DELETE
+    submitTask(basicHandle(message))
+    #submitTask(handleBot(message))
+    #try:
+    #  await handleText(message)
+    #except:
+    #  a = 1
+    #try:
+     #await handleTextG(message)
+    #except Exception:
+    #  print('h')
+
+    for words in censored_words:
+        if message.channel.nsfw:
             return
+        if isTextCensorEnabled == 1:
+            return
+        msg = message.content.lower().replace("0", "o").replace("4", "a").replace("3", "e").replace("@", "o").replace(
+            "1", "l").replace(".", "").replace(" ", "")
+        if words in unicodedata.normalize('NFKC', msg):
+            await message.delete()
+            #await message.channel.send("get censored")
 
-    if await checkVisual(message):
-        return
 
 if __name__ == '__main__':
     client.run(TOKEN)
