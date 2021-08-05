15f7b0af31dab652fac5b7f5d77e4d88188ad0de
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-08-05 19:06:36 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -3,24 +3,45 @@ from threading import Thread
 import urllib.request
 import os
 import time
+import random
 app = Flask('')
 hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)',
-        'Authorization': os.getenv('o7APIKey'),
+        #'Authorization': os.getenv('o7APIKey'),
  }
-url = "https://o7-api.glitch.me/api/json/graphical/classification/https://github.com/o7-Fire/General/raw/master/Human/Logo/o7A.png"
-@app.route('/')
+url = os.getenv("o7API") + "/api/json/graphical"
+
+
 def main():
-  return "Your bot is aliven't!<br>" + fetch(url) + '<br><img src="https://github.com/o7-Fire/General/raw/master/Human/Logo/o7A.png" alt="o7A.png">'
+  return fetch(url)
+
+def assad(path):
+  return path +" assad" + str(random.randint(60, 250))
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def all_routes(path):
+    if path.startswith('assad'):
+      return assad(path)
+    else:
+      return main()
+
 
 def fetch(url):
-  req = urllib.request.Request(url, headers=hdr)
-  response = urllib.request.urlopen(req)
-  return str(response.read().decode())
+  try:
+   req = urllib.request.Request(url, headers=hdr)
+   response = urllib.request.urlopen(req)
+   return str(response.read().decode())
+  except Exception as e:
+    return str(e)
 
 def alive():
+  print("ALIVEEEEEE")
   while True:
-    time.sleep(20)
-    fetch(url)
+    time.sleep(int(random.randint(16, 25)))
+    try:
+      print(fetch("https://o7-Fire-API-Java-Replit.o7fire.repl.co"))
+    except Exception as e:
+      print(e)
 
 def run():
   app.run(host="0.0.0.0", port=8080)
